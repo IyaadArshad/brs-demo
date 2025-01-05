a733c5fcbb5257c9618be730d6f1611097716012
Iyaad Arshad <iyaad.a_cis@gemselearning.com>
2025-01-05 08:10:34 +0530

    Update icon components to improve accessibility and enhance SVG structure

--- app/components/icons.tsx
+++ app/components/icons.tsx
@@ -56,7 +56,7 @@ export function ArrowLeftIcon() {
 
 export function AIIcon() {
   return (
-    <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" className="octicon octicon-code UnderlineNav-octicon d-none d-sm-inline" fill="#f6f6e7">
+    <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" fill="#f6f6e7">
     <path d="m11.28 3.22 4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.749.749 0 0 1-1.275-.326.749.749 0 0 1 .215-.734L13.94 8l-3.72-3.72a.749.749 0 0 1 .326-1.275.749.749 0 0 1 .734.215Zm-6.56 0a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042L2.06 8l3.72 3.72a.749.749 0 0 1-.326 1.275.749.749 0 0 1-.734-.215L.47 8.53a.75.75 0 0 1 0-1.06Z"></path>
 </svg>
   );
@@ -64,8 +64,8 @@ export function AIIcon() {
 
 export function GlossaryIcon() {
   return (
-    <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor">
-      <path d="M0 1.75A.75.75 0 0 1 .75 1h4.253c1.227 0 2.317.59 3 1.501A3.743 3.743 0 0 1 11.006 1h4.245a.75.75 0 0 1 .75.75v10.5a.75.75 0 0 1-.75.75h-4.507a2.25 2.25 0 0 0-1.591.659l-.622.621a.75.75 0 0 1-1.06 0l-.622-.621A2.25 2.25 0 0 0 5.258 13H.75a.75.75 0 0 1-.75-.75Zm7.251 10.324.004-8.074a.75.75 0 0 1 .363-.643 2.251 2.251 0 0 1 2.38 0 .75.75 0 0 1 .363.643l.004 8.074a.75.75 0 0 1-.26.571 3.732 3.732 0 0 1-2.594 1.043 3.732 3.732 0 0 1-2.594-1.043.75.75 0 0 1-.26-.57Z" />
-    </svg>
+    <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" fill="#f6f6e7">
+    <path d="M0 1.75C0 .784.784 0 1.75 0h12.5C15.216 0 16 .784 16 1.75v12.5A1.75 1.75 0 0 1 14.25 16H1.75A1.75 1.75 0 0 1 0 14.25ZM6.5 6.5v8h7.75a.25.25 0 0 0 .25-.25V6.5Zm8-1.5V1.75a.25.25 0 0 0-.25-.25H6.5V5Zm-13 1.5v7.75c0 .138.112.25.25.25H5v-8ZM5 5V1.5H1.75a.25.25 0 0 0-.25.25V5Z"></path>
+</svg>
   );
 }
