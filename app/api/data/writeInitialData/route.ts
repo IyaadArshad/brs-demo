9398b3b48cc7fcd2bf982a2b9efd71ee269ebec4
Iyaad Arshad <iyaad.a_cis@gemselearning.com>
2025-01-06 13:30:39 +0400

    refactor

--- app/api/data/writeInitialData/route.ts
+++ app/api/data/writeInitialData/route.ts
@@ -1,34 +1,53 @@
-import PocketBase from 'pocketbase';
+import PocketBase from "pocketbase";
 
-const pb = new PocketBase('http://127.0.0.1:8090');
+const pb = new PocketBase("http://127.0.0.1:8090");
 
 export async function GET(request: Request) {
-    const url = new URL(request.url);
-    const fileName = url.searchParams.get('file_name');
-    const fileData = url.searchParams.get('data');
+  const url = new URL(request.url);
+  const fileName = url.searchParams.get("file_name");
+  const fileData = url.searchParams.get("data");
 
-    if (!fileName || !fileData) {
-        return Response.json({ error: 'Missing required parameters' }, { status: 400 })
-    }
+  let id;
+  let idFetch;
+  try {
+    idFetch = await fetch("/api/data/fetchRecordId?file_name=" + fileName);
+    const idFetchJson = await idFetch.json();
+    id = idFetchJson.id;
+  } catch (e) {
+    console.error(e);
+  }
 
-    if (fileName.length > 500) {
-        return Response.json({ 
-            success: false,
-            message: "File name too long, pick a shorter name"
-         }, { status: 400 })
-    } else if (fileData.length > 100000000) {
-        return Response.json({ 
-            success: false,
-            message: "File data too large, let the user know the file is too large"
-         }, { status: 400 })
-    }
+  if (!fileName || !fileData) {
+    return Response.json(
+      { error: "Missing required parameters" },
+      { status: 400 }
+    );
+  }
 
-    const data = {
-        "file_name": fileName,
-        "data": fileData
-    };
-    
-    const record = await pb.collection('files').create(data);
+  if (fileName.length > 500) {
+    return Response.json(
+      {
+        success: false,
+        message: "File name too long, pick a shorter name",
+      },
+      { status: 400 }
+    );
+  } else if (fileData.length > 100000000) {
+    return Response.json(
+      {
+        success: false,
+        message: "File data too large, let the user know the file is too large",
+      },
+      { status: 400 }
+    );
+  }
 
-    return Response.json({ "hi": "ho" })
+  const data = {
+    file_name: fileName,
+    data: fileData,
+  };
+
+  const record = await pb.collection("files").create(data);
+
+  return Response.json({ success: "true" });
 }
