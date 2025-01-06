5fd6948d2a2a33ff7009f8defa7a0fd4ef874d8b
Iyaad Arshad <iyaad.a_cis@gemselearning.com>
2025-01-06 13:13:11 +0400

    Add PocketBase integration for file creation; update .gitignore and package dependencies

---
+++ app/api/data/createFile/route.ts
@@ -0,0 +1,34 @@
+import PocketBase from 'pocketbase';
+
+const pb = new PocketBase('http://127.0.0.1:8090');
+
+export async function GET(request: Request) {
+    const url = new URL(request.url);
+    const fileName = url.searchParams.get('file_name');
+    const fileData = url.searchParams.get('data');
+
+    if (!fileName || !fileData) {
+        return Response.json({ error: 'Missing required parameters' }, { status: 400 })
+    }
+
+    if (fileName.length > 500) {
+        return Response.json({ 
+            success: false,
+            message: "File name too long, pick a shorter name"
+         }, { status: 400 })
+    } else if (fileData.length > 1200000) {
+        return Response.json({ 
+            success: false,
+            message: "File data too large, let the user"
+         }, { status: 400 })
+    }
+
+    const data = {
+        "file_name": fileName,
+        "data": fileData
+    };
+    
+    const record = await pb.collection('files').create(data);
+
+    return Response.json({ "hi": "ho" })
+}
