01f41f585b6542d6654a5b02eddaee7e8d6a2bf6
Iyaad Arshad <iyaad.a_cis@gemselearning.com>
2025-01-06 13:24:01 +0400

    Enhance GET method to retrieve record ID by file name from query parameters

--- app/api/data/fetchRecordId/route.ts
+++ app/api/data/fetchRecordId/route.ts
@@ -3,7 +3,10 @@ import Pocketbase from 'pocketbase';
 const pb = new Pocketbase('http://127.0.0.1:8090');
 
 export async function GET(request: Request ) {
-    const record = await pb.collection('files').getFirstListItem('file_name="woo.md"', {fields: 'id'});
+    const url = new URL(request.url);
+    const file_name = url.searchParams.get('file_name');
+
+    const record = await pb.collection('files').getFirstListItem(`file_name='${file_name}'`, {fields: 'id'});
 
     return Response.json(record);
 }
