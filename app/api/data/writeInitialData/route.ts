e33055200637dc780e5e203bfabb3626fa5ff56e
Iyaad Arshad <iyaad.a_cis@gemselearning.com>
2025-01-06 17:31:32 +0400

    logoff -- plan on rewritting incredibly wrongly done writeInitialData

--- app/api/data/writeInitialData/route.ts
+++ app/api/data/writeInitialData/route.ts
@@ -6,39 +6,7 @@ export async function GET(request: Request) {
   const url = new URL(request.url);
   const fileName = url.searchParams.get("file_name");
   const fileData = url.searchParams.get("data");
-  const id = await fetch("/api/data/fetchRecordId?file_name=" + fileName);
-
-  if (!fileName || !fileData) {
-    return Response.json(
-      { error: "Missing required parameters" },
-      { status: 400 }
-    );
-  }
-
-  if (fileName.length > 500) {
-    return Response.json(
-      {
-        success: false,
-        message: "File name too long, pick a shorter name",
-      },
-      { status: 400 }
-    );
-  } else if (fileData.length > 100000000) {
-    return Response.json(
-      {
-        success: false,
-        message: "File data too large, let the user know the file is too large",
-      },
-      { status: 400 }
-    );
-  }
-
-  const data = {
-    file_name: fileName,
-    data: fileData,
-  };
-
-  const record = await pb.collection("files").update(String(id), data);
-
-  return Response.json({ success: "true" });
+  const id = await fetch(`${process.env.HOSTED}/api/data/fetchRecordId?file_name=` + fileName);
+  console.log(id);
+  return Response.json({ success: "true", id, file_name: fileName });
 }
