7be1f03e2f95fc17df4da56dcc48050a5e193cfe
Iyaad Arshad <iyaad.a_cis@gemselearning.com>
2025-01-08 09:01:13 +0400

    Added checkInit API route

--- app/api/data/checkInit/route.ts
+++ app/api/data/checkInit/route.ts
@@ -2,9 +2,36 @@ import PocketBase from "pocketbase";
 
 const pb = new PocketBase(`${process.env.POCKETBASE_SERVER_URL}`);
 
-export function GET(request: Request) {
-    return Response.json({
+export async function GET(request: Request) {
+  const url = new URL(request.url);
+  const file_name = url.searchParams.get("file_name");
+
+  try {
+    const record = await pb
+      .collection("files")
+      .getFirstListItem(`file_name='${file_name}'`, { fields: "data" });
+    if (String(record.data) === "0") {
+      return Response.json({
         success: true,
-        pb: `${process.env.POCKETBASE_SERVER_URL}`
-    })
+        initialized: false,
+        needsInitialWrite: true,
+      });
+    } else {
+      return Response.json({
+        success: true,
+        initialized: true,
+        needsInitialWrite: false,
+      });
+    }
+  } catch (error) {
+    return Response.json(
+      {
+        success: false,
+        error: "Record not found",
+        message:
+          "Please double check the file name and try again. Make sure you are using the correct file name",
+      },
+      { status: 404 }
+    );
+  }
 }
