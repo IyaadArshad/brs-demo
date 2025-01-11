6b40bba3f8beb794434527ab7384c5ed07f41b33
Iyaad Arshad <iyaad.a_cis@gemselearning.com>
2025-01-11 12:32:56 +0400

    Added test editor

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -1,12 +1,17 @@
 "use client";
 import { useState } from "react";
 import { CraftEditor, JSONContent } from "@sergeysova/craft";
+import './styles.css';
 
 export default function App() {
   const [content, setContent] = useState<JSONContent>({})
-
-  return <CraftEditor 
-    content={content} 
-    onUpdate={(editor: { getJSON: () => JSONContent }) => setContent(editor.getJSON())} 
-  />;
+  return (
+    <div style={{ backgroundColor: 'black' }}>
+      <CraftEditor 
+        content={content} 
+        onUpdate={(editor: { getJSON: () => JSONContent }) => setContent(editor.getJSON())}
+        className="white-text"
+      />
+    </div>
+  );
 }
