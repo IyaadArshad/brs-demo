10da94954da9073f831550d1df8864d854a3c8bf
Iyaad Arshad <iyaad.a_cis@gemselearning.com>
2025-01-27 12:34:57 +0400

    Changed heading of page

--- app/editor/layout.tsx
+++ app/editor/layout.tsx
@@ -1,8 +1,14 @@
 import './styles.css'
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from './theme-provider'
+import { Metadata } from 'next'
 
 const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: "BRS Document Editor - ChatGPT",
+  description: "no description",
+};
 
 export default function RootLayout({
   children,
